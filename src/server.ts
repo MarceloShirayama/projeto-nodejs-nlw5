f39a7b40e1be91e8cf9e652e1ea17656a367c6ab
MarceloShirayama <caixadecorreiodomarcelo@gmail>
2021-04-20 11:01:55 -0300

    🔧 add config typeorm and eslint

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import express from 'express';
+import './database';
 
 const app = express();
 
 app.get('/', (req, res) => {
-    // return res.send('<h1>Olá NLW5!!!</h1>');
-    return res.json({ message: 'Olá NLW5!!!'});
+  // return res.send('<h1>Olá NLW5!!!</h1>');
+  return res.json({ message: 'Olá NLW5!!!' });
 });
 
 app.post('/', (req, res) => {
-    return res.json({ message: 'Usuário cadastrado com sucesso'})
+  return res.json({ message: 'Usuário cadastrado com sucesso' })
 });
 
 app.listen(3333, () => console.log('listening on http://localhost:3333'));
